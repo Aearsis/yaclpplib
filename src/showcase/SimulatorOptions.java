b7788ac601a8a802116459e406ee11b4b61229d9
Salmelu <salmelu@gmail.com>
2017-05-18 22:10:05 +0200

    Added new OptionsClass test and showcase

---
+++ src/showcase/SimulatorOptions.java
@@ -0,0 +1,98 @@
+package showcase;
+
+import cz.cuni.mff.yaclpplib.OptionValue;
+import cz.cuni.mff.yaclpplib.Options;
+import cz.cuni.mff.yaclpplib.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimulatorOptions implements Options {
+
+    public class UserException extends RuntimeException {
+        UserException(String s) {
+            super(s);
+        }
+    }
+
+    public enum SimulationType {
+        EXPLOSION, ENERGY_SURGE, DIRECTIONAL
+    }
+
+    @Option("-r")
+    @Option("--repulsive")
+    @Help("uses the repulsive force between particles")
+    public boolean repulsiveForce = false;
+
+    @Option("-c")
+    @Option("--compulsive")
+    @Help("uses the compulsive force between particles")
+    public boolean compulsiveForce = false;
+
+    public int debugLevel = 0;
+
+    @Option("--debug")
+    @Help("enables debug outputs")
+    @OptionalValue
+    public void enableDebug(Integer level) {
+        if (level != null) {
+            debugLevel = level;
+        }
+        else {
+            debugLevel = 10;
+        }
+    }
+
+    @Option("-m")
+    @Option("--mass")
+    @Help("the mass of the particles")
+    public double particleMass = 1.0;
+
+    @Option("-d")
+    @Option("--decay")
+    @Help("decay of particles speed")
+    public double decay = 0.01;
+
+    @Option("-i")
+    @Option("--iterations")
+    @Help("number of iterations calculated")
+    @Range(minimumValue = 1, maximumValue = Integer.MAX_VALUE)
+    @Mandatory
+    public int iterations;
+
+    @Option("-p")
+    @Option("-c")
+    @Option("--count")
+    @Help("amount of simulated particles")
+    @Range(minimumValue = 1, maximumValue = 10000)
+    @Mandatory
+    public int particles;
+
+    @Option("-s")
+    @Option("--simulate")
+    @Help("runs specified simulation")
+    @Mandatory
+    public SimulationType[] simulations;
+
+    @Option("-o")
+    @Option("--output")
+    public String outputFile = null;
+
+    // Positional arguments
+    public List<String> inputs = new ArrayList<>();
+
+    @AfterParse
+    public void checkEnabled() {
+        if (!repulsiveForce && !compulsiveForce) {
+            throw new UserException("One of the forces must be enabled");
+        }
+    }
+
+    @AfterParse
+    public void checkAnyInputs() {
+        if (inputs.size() == 0) {
+            throw new UserException("No input files specified");
+        }
+    }
+
+}
---
+++ test/cz/cuni/mff/yaclpplib/parserTests/SimulatiorOptionsTest.java
@@ -0,0 +1,84 @@
+package cz.cuni.mff.yaclpplib.parserTests;
+
+import cz.cuni.mff.yaclpplib.ArgumentParser;
+import cz.cuni.mff.yaclpplib.ArgumentParserFactory;
+import cz.cuni.mff.yaclpplib.InvalidOptionValue;
+import cz.cuni.mff.yaclpplib.MissingMandatoryOptionException;
+import org.junit.Before;
+import org.junit.Test;
+import showcase.SimulatorOptions;
+
+import static org.junit.Assert.*;
+
+public class SimulatiorOptionsTest {
+    private ArgumentParser parser;
+    private SimulatorOptions options;
+
+    @Before
+    public void setUp() {
+        options = new SimulatorOptions();
+        parser = ArgumentParserFactory.create();
+        parser.addOptions(options);
+        parser.setUnexpectedParameterHandler(options.inputs::add);
+    }
+
+    @Test(expected = SimulatorOptions.UserException.class)
+    public void testNoForceSelected() {
+        parser.parse("-i 5 --count=100 -s EXPLOSION input1".split(" "));
+    }
+
+    @Test(expected = SimulatorOptions.UserException.class)
+    public void testMissingInput() {
+        parser.parse("-i 5 -c --count=100 -s EXPLOSION".split(" "));
+    }
+
+    @Test(expected = InvalidOptionValue.class)
+    public void testOutOfRangeNegative() {
+        parser.parse("-i 5 -r --count=-3 -s EXPLOSION input1".split(" "));
+    }
+
+    @Test(expected = InvalidOptionValue.class)
+    public void testOutOfRangeHigh() {
+        parser.parse("-i 5 -c --count=1000000 -s EXPLOSION input1".split(" "));
+    }
+
+    @Test(expected = MissingMandatoryOptionException.class)
+    public void testMissingMandatory() {
+        parser.parse("-c --count=1000000 -s EXPLOSION input1".split(" "));
+    }
+
+    @Test
+    public void testEmptyDebug() {
+        parser.parse("-i 5 -c --count=1000000 -s --debug EXPLOSION input1".split(" "));
+
+        assertEquals(options.debugLevel, 10);
+    }
+
+    @Test
+    public void testValueDebug() {
+        parser.parse("-i 5 -c --count=1000000 -s --debug=20 EXPLOSION input1".split(" "));
+
+        assertEquals(options.debugLevel, 20);
+    }
+
+    @Test
+    public void testValidInput() {
+        parser.parse(("-i 10 -c -r --count=50 -s DIRECTIONAL -s EXPLOSION -m 2.50 -d 0.1 " +
+                "--output=logfile input1 input2 input3").split(" "));
+
+        assertEquals(options.iterations, 10);
+        assertEquals(options.compulsiveForce, true);
+        assertEquals(options.repulsiveForce, true);
+        assertEquals(options.particles, 50);
+        assertEquals(options.simulations.length, 1);
+        assertEquals(options.outputFile, "logfile");
+        assertEquals(options.particleMass, 2.5, 0.0000001);
+        assertEquals(options.particleMass, 0.1, 0.0000001);
+        assertEquals(options.simulations[0], SimulatorOptions.SimulationType.DIRECTIONAL);
+        assertEquals(options.simulations[1], SimulatorOptions.SimulationType.EXPLOSION);
+        assertEquals(options.inputs.get(0), "input1");
+        assertEquals(options.inputs.get(1), "input2");
+        assertEquals(options.inputs.get(2), "input3");
+    }
+
+}
